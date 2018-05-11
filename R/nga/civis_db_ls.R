0ace41078c160ebca0ac314b0ed18b538f62dbce
josiahparry <josiah.parry@gmail.com>
2018-05-10 21:16:53 -0400

    add tidy listing of tables

--- R/nga/civis_db_ls.R
+++ R/nga/civis_db_ls.R
@@ -1,4 +1,4 @@
-# List databases in civis
+# List all databases in civis
 civis_db_ls <- function() {
   httr::RETRY("GET", url = paste0("https://api.civisanalytics.com/databases"),
               authenticate(api_key(), "")) %>%
@@ -8,15 +8,28 @@ civis_db_ls <- function() {
     as_tibble()
 }
 
+# list all tables
+civis_table_ls <- function() {
+  httr::RETRY("GET", url = paste0("https://api.civisanalytics.com/tables"),
+              authenticate(api_key(), "")) %>%
+    httr::content(type = "text") %>%
+    jsonlite::fromJSON() %>%
+    rename(db_id = id) %>%
+    jsonlite::flatten() %>%
+    as_tibble()
+}
 
 # Create pipeable function that provides a data frame of databases and their schemas
-civis_db_schemas <- function(dbs) {
+add_schemas <- function(dbs) {
   dbs %>%
     mutate(schema_url = paste(base_url(), "databases", db_id, "schemas", sep = "/")) %>%
     mutate(schemas = map(schema_url, get_civis_api)) %>%
     select(-schema_url)
-
 }
+
+# create a pipeable function that provides a database of tables based on provided schema names
+# This is intended to come from either db or schema
+
 
 
 # Create a function that will perform get civis requests
